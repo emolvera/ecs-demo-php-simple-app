ca2ffd056944d6b3ed2c8d478c9ab2138ede7027
emmoh <emmoh@github.com>
2021-05-07 12:09:46 -0500

    Version 1.1

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Hello World!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #323b87;}</style>
+        <style>body {margin-top: 40px; background-color: #32873b;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Hello World!</h1>
-                <h2>This is version 1.1</h2>
+                <h2>This is version 1.2</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
